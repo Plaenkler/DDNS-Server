2bbaa3e0630978f236af56b697c45879a7131ea7
Simon Lukas ~ Znox / Plänkler <60503970+Plaenkler@users.noreply.github.com>
2023-04-08 00:57:28 +0200

    [UPD] Refactored IP functions
    - Removed unnecessary IPv4/6 validation functions
    - Private subnet check simplified
    - Optimized GetCallerIP -> return the first valid IP address found

--- pkg/nswrapper/ip.go
+++ pkg/nswrapper/ip.go
@@ -6,103 +6,74 @@ import (
 	"net"
 	"net/http"
 	"strings"
-
-	"github.com/labstack/gommon/log"
-	"github.com/plaenkler/ddns-server/pkg/ipparser"
 )
 
-// GetIPType finds out if the IP is IPv4 or IPv6
+// Returns the type of IP address (A or AAAA) for a given IP address.
+// If the IP address is not valid, an empty string is returned.
 func GetIPType(ipAddr string) string {
-	if ipparser.ValidIP4(ipAddr) {
-		return "A"
-	} else if ipparser.ValidIP6(ipAddr) {
-		return "AAAA"
-	} else {
+	ip := net.ParseIP(ipAddr)
+	if ip == nil {
 		return ""
 	}
+	if ip.To4() != nil {
+		return "A"
+	}
+	if ip.To16() != nil {
+		return "AAAA"
+	}
+	return ""
 }
 
-// GetCallerIP searches for the "real" IP senders has actually.
-// If its a private address we won't use it.
+// Returns the public IP address of the client making the HTTP request.
+// If a public IP address cannot be found, an error is returned.
 func GetCallerIP(r *http.Request) (string, error) {
-	log.Info("request", r.Header)
 	for _, h := range []string{"X-Real-Ip", "X-Forwarded-For"} {
 		addresses := strings.Split(r.Header.Get(h), ",")
-		// march from right to left until we get a public address
-		// that will be the address right before our proxy.
 		for i := len(addresses) - 1; i >= 0; i-- {
 			ip := strings.TrimSpace(addresses[i])
-			// header can contain spaces too, strip those out.
 			realIP := net.ParseIP(ip)
-			if !realIP.IsGlobalUnicast() || isPrivateSubnet(realIP) {
-				// bad address, go to next
+			if realIP == nil {
 				continue
 			}
-			return ip, nil
+			if realIP.IsGlobalUnicast() && !isPrivateSubnet(realIP) {
+				return ip, nil
+			}
 		}
 	}
-	return "", errors.New("no match")
+	return "", errors.New("unable to determine caller IP address")
 }
 
-// ShrinkUserAgent simply cuts the user agent information if its too long to display.
 func ShrinkUserAgent(agent string) string {
-	agentParts := strings.Split(agent, " ")
-
-	return agentParts[0]
+	parts := strings.Fields(agent)
+	return parts[0]
 }
 
-// ipRange - a structure that holds the start and end of a range of ip addresses
-type ipRange struct {
-	start net.IP
-	end   net.IP
+type IPRange struct {
+	start, end net.IP
 }
 
-// inRange - check to see if a given ip address is within a range given
-func inRange(r ipRange, ipAddress net.IP) bool {
-	// strcmp type byte comparison
-	if bytes.Compare(ipAddress, r.start) >= 0 && bytes.Compare(ipAddress, r.end) < 0 {
-		return true
-	}
-	return false
+func isInRange(r IPRange, ip net.IP) bool {
+	return bytes.Compare(ip, r.start) >= 0 && bytes.Compare(ip, r.end) < 0
 }
 
-var privateRanges = []ipRange{
-	ipRange{
-		start: net.ParseIP("10.0.0.0"),
-		end:   net.ParseIP("10.255.255.255"),
-	},
-	ipRange{
-		start: net.ParseIP("100.64.0.0"),
-		end:   net.ParseIP("100.127.255.255"),
-	},
-	ipRange{
-		start: net.ParseIP("172.16.0.0"),
-		end:   net.ParseIP("172.31.255.255"),
-	},
-	ipRange{
-		start: net.ParseIP("192.0.0.0"),
-		end:   net.ParseIP("192.0.0.255"),
-	},
-	ipRange{
-		start: net.ParseIP("192.168.0.0"),
-		end:   net.ParseIP("192.168.255.255"),
-	},
-	ipRange{
-		start: net.ParseIP("198.18.0.0"),
-		end:   net.ParseIP("198.19.255.255"),
-	},
+var privateRanges = []IPRange{
+	{start: net.ParseIP("10.0.0.0"), end: net.ParseIP("10.255.255.255")},
+	{start: net.ParseIP("100.64.0.0"), end: net.ParseIP("100.127.255.255")},
+	{start: net.ParseIP("172.16.0.0"), end: net.ParseIP("172.31.255.255")},
+	{start: net.ParseIP("192.0.0.0"), end: net.ParseIP("192.0.0.255")},
+	{start: net.ParseIP("192.168.0.0"), end: net.ParseIP("192.168.255.255")},
+	{start: net.ParseIP("198.18.0.0"), end: net.ParseIP("198.19.255.255")},
 }
 
-// isPrivateSubnet - check to see if this ip is in a private subnet
+// Checks whether the passed IP address is in a private subnet.
+// Returns true if the IP address is in a private subnet.
 func isPrivateSubnet(ipAddress net.IP) bool {
-	// my use case is only concerned with ipv4 atm
-	if ipCheck := ipAddress.To4(); ipCheck != nil {
-		// iterate over all our ranges
-		for _, r := range privateRanges {
-			// check if this ip is in a private range
-			if inRange(r, ipAddress) {
-				return true
-			}
+	if ipCheck := ipAddress.To4(); ipCheck == nil {
+		return false
+	}
+	for _, r := range privateRanges {
+		if isInRange(r, ipAddress) {
+			return true
 		}
 	}
 	return false
